987770497db2c6d131c36771e58fc73d0f3968f3
Omer <hbaharkus@gmail.com>
2020-02-05 18:30:27 +0100

    fixed everything

--- scripts/listener.js
+++ scripts/listener.js
@@ -1,5 +1,1 @@
-function calc() {
-    var a = parseInt(document.querySelector("#value1").value);
-    var b = parseInt(document.querySelector("#value2").value);
-    var op = document.querySelector("#operator").value;
-    var calculate;
+document.getElementById("calculatebutton").addEventListener("click", calc);
