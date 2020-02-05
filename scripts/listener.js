6bc193194951301ca52e9abb6c98328a81989f1b
Omer <hbaharkus@gmail.com>
2020-02-05 02:07:52 +0100

    e

--- scripts/listener.js
+++ scripts/listener.js
@@ -1,5 +1,5 @@
 function calc() {
     var a = parseInt(document.querySelector("#value1").value);
-    var b = parseInt(document.querySelector("#valu2").value);
+    var b = parseInt(document.querySelector("#value2").value);
     var op = document.querySelector("#operator").value;
     var calculate;
